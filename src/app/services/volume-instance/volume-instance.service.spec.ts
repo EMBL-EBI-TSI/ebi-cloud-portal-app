09051e0ec3aacd112b0b28744d32079374021bf8
Jose A Dianes <jadianes@gmail.com>
2016-03-17 10:58:30 +0000

    added basic testing harness + 1 test to VolumeInstanceService

--- src/app/services/volume-instance/volume-instance.service.spec.ts
+++ src/app/services/volume-instance/volume-instance.service.spec.ts
@@ -44,13 +44,37 @@ describe('VolumeInstanceService', () => {
     ErrorService
   ]);
 
-  it('should get account data',
-    inject([VolumeInstanceService, CredentialService, MockBackend],
-      fakeAsync(
-        (volumeInstanceService, credentialService, mockBackend) => {
-        }
+  it('should get all instances',
+      inject([VolumeInstanceService, CredentialService, MockBackend],
+          fakeAsync(
+              (volumeInstanceService, credentialService, mockBackend) => {
+                  var res;
+                  mockBackend.connections.subscribe(c => {
+                      expect(c.request.url).toBe('some_url/volumeinstance/');
+                      let response = new ResponseOptions({
+                          body: `{"_embedded": {"volumeInstanceResourceList": [
+                            {"reference": "tsi-ref-1",
+                            "name": "vol_name",
+                            "providerId": "provider-id-1"},
+                            {"reference": "tsi-ref-2",
+                            "name": "vol_name",
+                            "providerId": "provider-id-2"}
+                            ]}}`
+                      });
+                      c.mockRespond(new Response(response));
+                  });
+
+                  credentialService.setCredentials('username', 'userpassword');
+                  volumeInstanceService.getAll(credentialService).subscribe(
+                      (_res) => {
+                          res = _res;
+                      }
+                  );
+                  tick();
+                  expect(res.length).toBe(2);
+              }
+          )
       )
-    )
   );
 
 });
