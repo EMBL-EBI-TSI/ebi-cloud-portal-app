2e110eaf2e255d85e072bb1c302d96a1886c3890
Jose A Dianes <jadianes@gmail.com>
2016-07-12 11:29:19 +0100

    change api location for PROD

--- src/app/index.ts
+++ src/app/index.ts
@@ -14,7 +14,7 @@ let activeConfig: ConfigService;
 
 if ('production' === process.env.ENV) {
   enableProdMode();
-  activeConfig = new ConfigService('https://dev.tsi.ebi.ac.uk:8143/');
+  activeConfig = new ConfigService('https://dev.api.portal.tsi.ebi.ac.uk/');
 } else {
   //ENV_PROVIDERS.push(ELEMENT_PROBE_PROVIDERS);
   activeConfig = new ConfigService('http://localhost:8080/');
