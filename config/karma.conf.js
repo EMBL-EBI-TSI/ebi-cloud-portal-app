6472177cb0cc86f7706e3eab6b04f0bdf0182324
Jose A Dianes <jadianes@gmail.com>
2016-07-13 11:53:09 +0100

    removed firefox

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -81,8 +81,7 @@ module.exports = function(config) {
      * available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
      */
     browsers: [
-      'Chrome',
-      'Firefox'
+      'Chrome'
     ],
 
     /*
