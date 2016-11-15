4e912f89513b96e9fab1d21aae2f9740b7d3b47c
Jose A Dianes <jadianes@gmail.com>
2016-11-15 15:09:26 +0000

    added status-based pipe

--- src/app/pages/deployments-page/deployments-page.component.ts
+++ src/app/pages/deployments-page/deployments-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { DeploymentsComponent } from 'ng2-cloud-portal-presentation-lib';
+import { DeploymentStatusPipe } from './deployment-status.pipe';
 
 @Component({
   selector: 'deployments-page',
   directives: [ DeploymentsComponent ],
+  pipes: [ DeploymentStatusPipe ],
   styles: [require('./deployments-page.style.css')],
   template: require('./deployments-page.template.html')
 })
