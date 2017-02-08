9434c16b63f7e4721915a3846782de4c30a58414
Jose A Dianes <jadianes@gmail.com>
2017-02-08 13:07:34 +0000

    changing landing page to repository

--- src/app/pages/welcome-page/welcome-page.component.ts
+++ src/app/pages/welcome-page/welcome-page.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { CORE_DIRECTIVES } from '@angular/common';
 import { Router } from '@angular/router';
-import { LoginPage } from '../login-page';
 import { AboutPage } from '../about-page';
+import { RepositoryPage } from '../repository-page';
 import { TokenService } from 'ng2-cloud-portal-service-lib';
 
 @Component({
   selector: 'welcome-page',
   providers: [ ],
-  directives: [ CORE_DIRECTIVES, LoginPage, AboutPage ],
+  directives: [ CORE_DIRECTIVES, RepositoryPage, AboutPage ],
   pipes: [ ],
   styles: [require('./welcome-page.style.css')],
   template: require('./welcome-page.template.html')
