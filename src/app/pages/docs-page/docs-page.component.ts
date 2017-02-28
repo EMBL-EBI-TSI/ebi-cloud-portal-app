ee76676385b4efd2d5bd7bab625a64016e0c6c8d
navisatebi <navis@ebi.ac.uk>
2017-02-28 16:44:04 +0000

    adding snapshot of images for user documentation

--- src/app/pages/docs-page/docs-page.component.ts
+++ src/app/pages/docs-page/docs-page.component.ts
@@ -6,12 +6,36 @@ import { Component } from '@angular/core';
   template: require('./docs-page.template.html')
 })
 export class DocsPage {
-  ebiLogoWhite = 'assets/img/EMBL_EBI_Logo_white.png';
-  ebiLogoBlack = 'assets/img/EMBL_EBI_Logo_black.png';
-  ebiLogoLarge = 'assets/img/embl-logo.png';
-  ebiLogoOnly = 'assets/img/embl_logo_only.png';
-  elixirLogo = 'assets/img/elixir_logo.png';
-  cloudsLogo = 'assets/img/clouds_logo.png';
+
+ 	homeMenu = 'assets/img/home_menu.png';
+	
+  	loginUserPassword = 'assets/img/login_normal.png';
+  	loginSSOStepOne = 'assets/img/login_sso_step_one.png';
+  	loginSSOStepTwo = 'assets/img/login_sso_step_two.png';
+  	
+  	homeProfile = 'assets/img/profile_menu.png';
+  	
+  	profileAddCppStepOne = 'assets/img/profile_add_cpp_step_one.png';
+  	profileAddCppStepTwo  = 'assets/img/profile_add_cpp_step_two.png';
+  	profileAddedCpp = 'assets/img/profile_after_adding_cpp.png';
+  	
+
+ 	profileAddTeam = 'assets/img/profile_add_team.png';
+  	profileAfterAddingTeam = 'assets/img/profile_after_adding_team.png';
+  	profileAddTeamMembers = 'assets/img/profile_add_team_members.png';
+  	profileAfterAddingTeamMember = 'assets/img/profile_after_adding_team_member.png';
+  	
+  	profileSharingCpp = 'assets/img/profile_sharing_cpp.png';
+  	
+  	homeRepository = 'assets/img/home_repository.png';
+  	
+  	repositoryAddedApplication = 'assets/img/repository_added_application.png';
+  	repositoryAddingApplication= 'assets/img/repository_adding_application.png';
+  	
+  	repositoryToShareApplication = 'assets/img/repository_application_share.png';
+  	repositorySharingApplication = 'assets/img/repository_sharing_application.png';
+  
+  
   constructor() {
 
   }
