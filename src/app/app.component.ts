e66fed511ed0a5de14525deeafcc034644b904b4
Jose A Dianes <jadianes@gmail.com>
2017-04-10 15:10:33 +0100

    added credential menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { TokenService, CredentialService } from 'ng2-cloud-portal-service-lib';
+import { TokenService, CredentialService, CloudProviderParameters,
+  CloudProviderParametersService, ErrorService, AccountService } from 'ng2-cloud-portal-service-lib';
 import { Router } from '@angular/router';
 
 @Component({
@@ -14,11 +15,27 @@ export class AppComponent {
   name = 'Cloud Portal';
   ebiUrl = 'http://www.ebi.ac.uk/';
   tsiGithubUrl = 'https://github.com/EMBL-EBI-TSI';
+  loggedInAccount: Account;
+  cloudProviderParameters: CloudProviderParameters[];
+  sharedCloudProviderParameters: CloudProviderParameters[];
 
   constructor(public tokenService: TokenService,
         public credentialService: CredentialService,
+        public accountService: AccountService,
+        public cloudProviderParametersService: CloudProviderParametersService,
+        public errorService: ErrorService,
         public router: Router) {
-
+        if (tokenService.getToken()) {
+            this.accountService.getAccount(
+                this.credentialService.getUsername(),
+                this.tokenService.getToken()
+            ).subscribe(
+                (account) => {
+                    this.loggedInAccount = account;
+                }
+            );
+            this.updateCloudProviders(true);
+        }
   }
 
   logOut() {
@@ -26,5 +43,56 @@ export class AppComponent {
     this.tokenService.clearToken();
     this.router.navigateByUrl('/welcome');
   }
+
+  public setCurrentlySelectedCloudProviderParameters(cloudProviderParameters: CloudProviderParameters) {
+    console.log("Set provider to %O", cloudProviderParameters);
+    this.cloudProviderParametersService.currentlySelectedCloudProviderParameters = cloudProviderParameters;
+  }
+
+  public updateCloudProviders(open:boolean):void {
+        if (open) {
+            this.cloudProviderParametersService.getAll(
+                this.credentialService.getUsername(),
+                this.tokenService.getToken())
+            .subscribe(
+                cloudProviderParameters => {
+                    console.log('[App] cloud provider parameters data is %O', cloudProviderParameters);
+                    this.cloudProviderParameters = cloudProviderParameters
+                },
+                error => {
+                    console.log('[App] error %O', error);
+                    if (error[0]) {
+                        error = error[0];
+                    }
+                    this.errorService.setCurrentError(error);
+                    this.router.navigateByUrl('/error');
+                },
+                () => {
+                    console.log('[App] Cloud provider parameters data retrieval complete');
+                }
+            );
+
+            this.cloudProviderParametersService.getAllShared(
+                this.credentialService.getUsername(),
+                this.tokenService.getToken())
+            .subscribe(
+                cloudProviderParameters => {
+                    console.log('[App] shared cloud provider parameters data is %O', cloudProviderParameters);
+                    this.sharedCloudProviderParameters = cloudProviderParameters
+                },
+                error => {
+                    console.log('[App] error %O', error);
+                    if (error[0]) {
+                        error = error[0];
+                    }
+                    this.errorService.setCurrentError(error);
+                    this.router.navigateByUrl('/error');
+                },
+                () => {
+                    console.log('[App] shared Cloud provider parameters data retrieval complete');
+                }
+            );
+        }
+    }
 
 }
