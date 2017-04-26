a26305c7b7443f61f2a04751f9ef831124a31e46
Jose A Dianes <jadianes@gmail.com>
2017-04-26 11:06:36 +0100

    using add from repo for volume setups

--- src/app/pages/volumes-page/volumes-page.component.ts
+++ src/app/pages/volumes-page/volumes-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MdDialog } from '@angular/material';
 import { BreadcrumbService } from '../../services/breadcrumb/breadcrumb.service';
+import { VolumesComponent } from 'ng2-cloud-portal-presentation-lib';
+import { AddRepoDialog } from '../../dialogs/add-repo-dialog/add-repo-dialog.component';
 
 @Component({
   selector: 'volumes-page',
@@ -9,7 +12,8 @@ import { BreadcrumbService } from '../../services/breadcrumb/breadcrumb.service'
 })
 export class VolumesPageComponent implements OnInit {
 
-  constructor(public breadcrumbService: BreadcrumbService) {  }
+  constructor(public breadcrumbService: BreadcrumbService,
+    public dialog: MdDialog) {  }
 
   ngOnInit() {
     this.breadcrumbService.breadcrumb.push(
@@ -21,4 +25,11 @@ export class VolumesPageComponent implements OnInit {
     this.breadcrumbService.breadcrumb = [];
   }
 
+  openAddVolumeSetupDialog(volumesComponent: VolumesComponent) {
+    let dialogRef = this.dialog.open(AddRepoDialog);
+    dialogRef.afterClosed().subscribe(repoUri => {
+      if (repoUri)
+        volumesComponent.addVolumeSetup({ repoUri: repoUri });
+    });
+  }
 }
