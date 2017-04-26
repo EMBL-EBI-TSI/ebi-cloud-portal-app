97787c8fff2b4fef601d2583d3c6e94373fa51c7
Jose A Dianes <jadianes@gmail.com>
2017-04-26 10:11:50 +0100

    added auto-completion to share credentials dialog

--- src/app/pages/cloud-provider-parameters-page/cloud-provider-parameters-page.component.ts
+++ src/app/pages/cloud-provider-parameters-page/cloud-provider-parameters-page.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MdDialog, MdDialogRef } from '@angular/material';
 import { BreadcrumbService } from '../../services/breadcrumb/breadcrumb.service';
 import { CloudProviderParametersComponent } from 'ng2-cloud-portal-presentation-lib';
+import 'rxjs/add/operator/startWith';
+import { TeamService, TokenService, ErrorService } from 'ng2-cloud-portal-service-lib';
 
 @Component({
   selector: 'app-cloud-provider-parameters-page',
@@ -27,7 +30,7 @@ export class CloudProviderParametersPageComponent implements OnInit {
   }
 
   openShareCloudProviderDialog(cloudProviderDetail: CloudProviderParametersComponent) {
-    let dialogRef = this.dialog.open(ShareCloudProviderDialog);
+    let dialogRef = this.dialog.open(ShareCloudProviderDialog, {data: ['hola']});
     dialogRef.afterClosed().subscribe(shareWith => {
       if (shareWith)
         cloudProviderDetail.share(shareWith);
@@ -41,6 +44,43 @@ export class CloudProviderParametersPageComponent implements OnInit {
 })
 export class ShareCloudProviderDialog {
   robby = 'assets/img/Robby_form0.5x.png';
+  shareWithCtrl: FormControl;
+  filteredTeams: any;
+  teamNames: string[];
 
-  constructor(public dialogRef: MdDialogRef<ShareCloudProviderDialog>) { }
+  constructor(public teamService: TeamService,
+              public tokenService: TokenService,
+              public errorService: ErrorService,
+              private _router: Router,
+              public dialogRef: MdDialogRef<ShareCloudProviderDialog>) { 
+    this.shareWithCtrl = new FormControl();
+    this.teamService.getAllMember(this.tokenService.getToken())
+      .subscribe(
+      teams => {
+        console.log('[ShareDialog] member teams data is %O', teams);
+        this.teamNames = teams.map(team => team.name);
+        this.filteredTeams = this.shareWithCtrl.valueChanges
+        .startWith(null)
+        .map(team => this.filterTeams(team));
+      },
+      error => {
+        console.log('[Profile] error %O', error);
+        if (error[0]) {
+          error = error[0];
+        }
+        this.errorService.setCurrentError(error);
+        this._router.navigateByUrl('/error');
+      },
+      () => {
+          console.log('[ShareDialog] Shared Cloud provider parameters data retrieval complete');
+      }
+    );
+    
+  }
+
+  filterTeams(val: string) {
+    return val ? this.teamNames.filter(s => new RegExp(`^${val}`, 'gi').test(s))
+               : this.teamNames;
+  }
+
 }
