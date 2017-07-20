4294a2ac3bca378926b1355180e6068cdc022e7d
Jose A Dianes <jadianes@gmail.com>
2017-07-20 14:37:51 +0100

    more tests fixed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,38 @@
 import { TestBed, async, fakeAsync, tick} from '@angular/core/testing';
-import {BaseRequestOptions, ConnectionBackend, Http, RequestOptions} from '@angular/http';
-import {Response, ResponseOptions} from '@angular/http';
-import {MockBackend, MockConnection} from '@angular/http/testing';
-
+import { BaseRequestOptions, ConnectionBackend, Http, RequestOptions } from '@angular/http';
+import { Response, ResponseOptions } from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { RouterTestingModule } from "@angular/router/testing";
+import { Router } from "@angular/router";
 import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { MaterialModule, MdSelectModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { TokenService, CredentialService, AccountService,
-    ConfigService, ErrorService } from 'ng2-cloud-portal-service-lib';
+    ConfigService, ErrorService, CloudProviderParametersService } from 'ng2-cloud-portal-service-lib';
+import { BreadcrumbService } from './services/breadcrumb/breadcrumb.service';
+
+class MockConfig {
+  
+}
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ MaterialModule, MdSelectModule, RouterModule, CommonModule, FormsModule ],
+      imports: [ 
+        MaterialModule, MdSelectModule, RouterModule, CommonModule, FormsModule,
+        RouterTestingModule ],
       declarations: [
         AppComponent
       ],
       providers: [ 
-        TokenService, CredentialService, AccountService, ConfigService, ErrorService,
-        {provide: ConnectionBackend, useClass: MockBackend},
-        {provide: RequestOptions, useClass: BaseRequestOptions},
+        TokenService, CredentialService, AccountService,
+        ErrorService, CloudProviderParametersService,
+        BreadcrumbService,
+        { provide: ConfigService, useClass: MockConfig },
+        { provide: ConnectionBackend, useClass: MockBackend },
+        { provide: RequestOptions, useClass: BaseRequestOptions },
         Http 
       ]
     }).compileComponents();
--- src/app/pages/application-page/application-page.component.spec.ts
+++ src/app/pages/application-page/application-page.component.spec.ts
@@ -1,21 +1,55 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed, async, fakeAsync, tick, ComponentFixture } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { BaseRequestOptions, ConnectionBackend, Http, RequestOptions } from '@angular/http';
+import { Response, ResponseOptions } from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { FormsModule } from '@angular/forms';
 import { ApplicationPageComponent } from './application-page.component';
 import { BreadcrumbService } from '../../services/breadcrumb/breadcrumb.service';
 import { CommonModule } from '@angular/common';
-import { MdChipsModule, MdIconModule, MdSelectModule,
-    MdInputModule, MdCoreModule } from '@angular/material';
+import { 
+  MdChipsModule, MdIconModule, MdSelectModule,
+  MdInputModule, MdCoreModule, MdTooltipModule,
+  MdDialogModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
+import { 
+  TokenService, CredentialService, AccountService,
+  ConfigService, ErrorService, CloudProviderParametersService } from 'ng2-cloud-portal-service-lib';
+import { ConfigurationFilterPipe } from 'ng2-cloud-portal-presentation-lib';
 
+class MockConfig {
+  
+}
+@Component({
+  selector: 'cp-application-component',
+  template: ''
+})
+class MockApplicationComponent {
+
+}
+    
 describe('ApplicationPageComponent', () => {
   let component: ApplicationPageComponent;
   let fixture: ComponentFixture<ApplicationPageComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ CommonModule, MdChipsModule, MdIconModule,
-          MdSelectModule, MdInputModule, MdCoreModule, FormsModule ],
-      declarations: [ ApplicationPageComponent ],
-      providers: [ BreadcrumbService ]
+      imports: [ 
+        CommonModule, RouterModule, FormsModule,
+        RouterTestingModule,
+        MdChipsModule, MdIconModule, MdCoreModule,
+        MdSelectModule, MdInputModule, MdDialogModule,
+        MdTooltipModule 
+      ],
+      declarations: [ ApplicationPageComponent, MockApplicationComponent, ConfigurationFilterPipe ],
+      providers: [ 
+        BreadcrumbService,
+        { provide: ConfigService, useClass: MockConfig },
+        { provide: ConnectionBackend, useClass: MockBackend },
+        { provide: RequestOptions, useClass: BaseRequestOptions },
+        Http 
+      ]
     })
     .compileComponents();
   }));
