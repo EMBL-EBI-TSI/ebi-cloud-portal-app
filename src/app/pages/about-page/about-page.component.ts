df9af5f034e475ca9713a2ff38284eb74de72cc5
gef <gef@ebi.ac.uk>
2022-05-11 15:44:36 +0100

    aap-tracking#237 : Swtich ELIXIR to LS in UI

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -13,7 +13,7 @@ export class AboutPageComponent implements OnInit {
   ebiLogoBlack = 'assets/img/EMBL_EBI_Logo_black.png';
   ebiLogoLarge = 'assets/img/embl-logo.png';
   ebiLogoOnly = 'assets/img/embl_logo_only.png';
-  elixirLogo = 'assets/img/elixir_logo.png';
+  lsLogo = 'assets/img/elsri_logo--wosub.png';
   cloudsLogo = 'assets/img/clouds_logo.png';
   ecpMainInfographic = 'assets/img/ecp_main_infographic.png';
 
